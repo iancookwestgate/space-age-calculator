d64c8fd87145483f3f87d39ad8106af542502179
Ian Cook Westgate <iwestgate@hotmail.com>
2019-03-15 10:44:12 -0700

    add html functionality so I can visually test the javascript as I edit.

--- src/main.js
+++ src/main.js
@@ -10,7 +10,22 @@ export class processAge {
     this.planet = planet
  }
 
+
+
  // thisAge() {
  //
  // }
 }
+
+$(document).ready(function() {
+  $("button").click(function() {
+    let userInput = ($("#input").val().split(",")).map(Number);
+
+    // calculating age only
+    let userStuff = new processAge(userInput[0], userInput[1], userInput[2]);
+    console.log(userStuff);
+    let martianAge = userStuff.age;
+    $(".output").text(martianAge);
+
+  })
+});
