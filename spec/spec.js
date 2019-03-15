9334b225f195f551ba48cd276031c91a6908aafe
Ian Cook Westgate <iwestgate@hotmail.com>
2019-03-15 10:25:36 -0700

    change spec.js to reflect more accurate math.

--- spec/spec.js
+++ spec/spec.js
@@ -2,6 +2,7 @@ import { processAge } from './../src/main.js';
 
 describe('Space Age Calculations', function() {
   let inputStuff;
+
   beforeEach(function() {
     let userInput = [25, 70, "Mars"];
     inputStuff = new processAge(userInput[0], userInput[1], userInput[2]);
@@ -17,13 +18,24 @@ describe('Space Age Calculations', function() {
     expect(inputStuff.planet).toEqual("Mars");
   });
   it("should calculate the user's age in Mars years", function() {
-    let marsAge = ((inputStuff.age)*1.88);
-    expect(marsAge).toEqual(47);
+    let marsAge = ((inputStuff.age)/1.88);
+    expect(marsAge).toEqual(13.297872340425533);
   });
   it("should calculate how many years the user has left to live on Mars", function() {
-    let marsAge = ((inputStuff.age)*1.88);
-    let marsExpectancy = (inputStuff.lifeExpect);
+    let marsAge = ((inputStuff.age)/1.88);
+    let marsExpectancy = (inputStuff.lifeExpect/1.88);
     let marsRemainder = (marsExpectancy - marsAge);
-    expect(marsRemainder).toEqual(23);
+    expect(marsRemainder).toEqual(23.93617021276596);
   });
+
+  // beforeEach(function() {
+  //   let userInput = [40, 70, "Mars"];
+  //   inputStuff = new processAge(userInput[0], userInput[1], userInput[2]);
+  // });
+  //
+  // it("should return how many years the user has exceeded life expectancy on Mars", function() {
+  //   let marsAge = ((inputStuff.age)*1.88);
+  //   let marsExpectancy = (inputStuff.lifeExpect);
+  //   let marsOverflow =
+  // });
 });
